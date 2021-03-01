0acfcb8c7e198f9fde6168fcb1a5fbccf5c6b9c2
corey gibson <cgibsonmm@gmail.com>
2021-03-01 11:56:35 -0500

    Homework ready

--- data.js
+++ data.js
@@ -1,33 +1,45 @@
 const data = [
   {
-    title: "music",
-    img:
+    title: "Fetch the Bolt Cutters",
+    artist: "Fiona Apple",
+    img_url:
       "https://media.pitchfork.com/photos/5fc902c696ab91ca523e2c33/master/w_1600%2Cc_limit/Fiona-Apple.jpg",
-  },
-  { title: "music" },
-  {
-    title: "music",
-    img:
-      "https://media.pitchfork.com/photos/5fc902c696ab91ca523e2c33/master/w_1600%2Cc_limit/Fiona-Apple.jpg",
+    releaseDate: "April 17, 2020",
   },
   {
-    title: "music",
-    img:
-      "https://media.pitchfork.com/photos/5fc902c696ab91ca523e2c33/master/w_1600%2Cc_limit/Fiona-Apple.jpg",
+    title: "The Slow Rush",
+    artist: "Tame Impala",
+    img_url:
+      "https://media.pitchfork.com/photos/5e41992daf0b890008c98164/1:1/w_320/The%20Slow%20Rush_Tame%20Impala.jpg",
+    releaseDate: "February 14 2020",
+  },
+
+  {
+    title: "The New Abnormal",
+    artist: "The Strokes",
+    img_url:
+      "https://media.pitchfork.com/photos/5e8ca8479a08f300084d48d7/1:1/w_320/The%20New%20Abnormal_The%20Strokes.jpg",
+    releaseDate: "April 10, 2020",
   },
   {
-    title: "music",
-    img:
-      "https://media.pitchfork.com/photos/5fc902c696ab91ca523e2c33/master/w_1600%2Cc_limit/Fiona-Apple.jpg",
+    title: "After Hours",
+    artist: "The Weeknd",
+    img_url:
+      "https://media.pitchfork.com/photos/5e6fcda64b101700083a93ce/1:1/w_320/After%20Hours_The%20Weeknd.jpg",
+    releaseDate: "March 20, 2020",
   },
   {
-    title: "music",
-    img:
-      "https://media.pitchfork.com/photos/5fc902c696ab91ca523e2c33/master/w_1600%2Cc_limit/Fiona-Apple.jpg",
+    title: "Folklore",
+    artist: "Taylor Swift",
+    img_url:
+      "https://media.pitchfork.com/photos/5f1e2abad421092dd8f6c7ca/1:1/w_320/Taylor_Swift_folklore.jpeg",
+    releaseDate: "July 27, 2020",
   },
   {
-    title: "music",
-    img:
-      "https://media.pitchfork.com/photos/5fc902c696ab91ca523e2c33/master/w_1600%2Cc_limit/Fiona-Apple.jpg",
+    title: "It Is What It Is",
+    artist: "Thundercat",
+    img_url:
+      "https://media.pitchfork.com/photos/5e8b38abffd4cb0008aae7e9/1:1/w_320/It%20Is%20What%20It%20Is_Thundercat.jpg",
+    releaseDate: "April 7, 2020",
   },
 ];
--- script.js
+++ script.js
@@ -12,32 +12,72 @@ const loadingTime = setTimeout(() => {
 // page loading
 console.log(data);
 
-// 1. create a var called cards and use a querySelectorAll to save all of the dom elements with the class name 'card'
-let loadingCards = document.querySelectorAll(".card");
+// 1. create a let called cards and use a querySelectorAll to save all of the dom elements with the class name 'card-container'
 
-// 2. create a var called 'cardList' that selects the ul with the class of card-list
-let cardList = document.querySelector(".card-list");
+// 2. create a let called 'cardList' that selects the ul with the class of card-list
 
 function removePlaceholderCards() {
   // 3. using a for loop loop through each of the loadingCards and remove them from the page.
-  for (let i = 0; i < loadingCards.length; i++) {
-    loadingCards[i].remove();
-  }
 }
 
 function displayLoadedData() {
-  data.forEach((item) => {
-    let card = document.createElement("div");
-    card.classList.add("card");
+  // 4. Using a for loop, loop through all of the items in the data array console.log each item
+  // taking a look inside of each object we can see the data we will be using.
 
-    let header = document.createElement("div");
-    header.classList.add("header");
 
-    let img = document.createElement("img");
-    img.setAttribute("src", item.img);
-    header.innerText = item.title;
-    card.appendChild(header);
-    card.appendChild(img);
-    cardList.appendChild(card);
-  });
+
+    // STAY INSIDE THE FOR LOOP We need to create a few elements to build the boiler plate for our styling
+
+    // 5. create a five new divs called and save each to a let
+    // div name should be cardContainer, card, filterDiv, cardHeader, and cardBody
+
+    // 6. using appendChild append cardContainer to the cardList let defined above
+    // if this works you will be able to inspect the page amd see that the cardList has a number of empty divs
+
+    // 7. using appendChild append the div card to cardContainer
+
+    // 8. using appendChild append the filterDiv to the card div
+
+    // we can now start to add details to our cards
+
+    // 9. set the inner text of cardHeader to the current data[i].title
+
+
+    // 10.using appendChild append cardHeader and cardBody to the filterDiv
+
+    // At this point we should have a bunch of divs with no styling and not a lot of detail.
+
+    // 11. using classList add the class 'card-container' to the let cardContainer
+
+    // 12. using classList add the class 'card' to the let card
+
+    // 13. using classList add the class 'filter' to the let filterDiv
+
+    // 14. using classList add the class 'header' to the let cardHeader
+
+    // 15. using classList add the class 'card-body' to the let cardBody
+
+    // 15. using the style property add the add a background image to the card
+
+    // BONUS SECTION //
+
+    /// UNCOMMENT TO ATTEMPT THE BONUS
+    // cardBody.classList.add("details");
+    // card.addEventListener("mouseenter", showDetails);
+    // card.addEventListener("mouseleave", removeDetails);
+
+    // try to display the rest of the details in the card body these are set to only display on hover. try to add some classes and style it how you would like
+
+    // bonus 2. Add your favorite albums to the data.js file. you can hard code your data there
+
+  // FOR LOOP SHOULD CLOSE HERE!
+}
+
+// Functions for bonus
+function showDetails() {
+  this.children[0].children[1].classList.remove("details");
+}
+
+function removeDetails() {
+  this.children[0].children[1].classList.add("details");
 }
