7321cf51565aa4e9aa0caf39fcfcd73828bc1fec
corey gibson <cgibsonmm@gmail.com>
2021-02-28 21:38:11 -0500

    work to styling

--- data.js
+++ data.js
@@ -1,1 +1,33 @@
-const data = [{ title: "music" }];
+const data = [
+  {
+    title: "music",
+    img:
+      "https://media.pitchfork.com/photos/5fc902c696ab91ca523e2c33/master/w_1600%2Cc_limit/Fiona-Apple.jpg",
+  },
+  { title: "music" },
+  {
+    title: "music",
+    img:
+      "https://media.pitchfork.com/photos/5fc902c696ab91ca523e2c33/master/w_1600%2Cc_limit/Fiona-Apple.jpg",
+  },
+  {
+    title: "music",
+    img:
+      "https://media.pitchfork.com/photos/5fc902c696ab91ca523e2c33/master/w_1600%2Cc_limit/Fiona-Apple.jpg",
+  },
+  {
+    title: "music",
+    img:
+      "https://media.pitchfork.com/photos/5fc902c696ab91ca523e2c33/master/w_1600%2Cc_limit/Fiona-Apple.jpg",
+  },
+  {
+    title: "music",
+    img:
+      "https://media.pitchfork.com/photos/5fc902c696ab91ca523e2c33/master/w_1600%2Cc_limit/Fiona-Apple.jpg",
+  },
+  {
+    title: "music",
+    img:
+      "https://media.pitchfork.com/photos/5fc902c696ab91ca523e2c33/master/w_1600%2Cc_limit/Fiona-Apple.jpg",
+  },
+];
--- script.js
+++ script.js
@@ -2,19 +2,42 @@
 function displayLoaded() {}
 // Do not modify
 const loadingTime = setTimeout(() => {
-  displayLoaded();
+  removePlaceholderCards();
+  displayLoadedData();
 }, 3000);
 
 // In order to complete this assignment you will have access to an array called data.
-// We will be mimicking a slow loading web site. To enhance use experience will will
+// We will be mimicking a slow loading web site. To enhance use experience we will
 // provide the user with some place holder cards and animations while we mimic the
 // page loading
 console.log(data);
 
-// 1. Save all of the cards as an array of dom elements
-let cards = document.querySelectorAll(".card");
+// 1. create a var called cards and use a querySelectorAll to save all of the dom elements with the class name 'card'
+let loadingCards = document.querySelectorAll(".card");
 
-// 2. using a for loop for each card in the cards array give the card the css class .loading-animation
-for (let i = 0; i < cards.length; i++) {
-  // cards[i].classList.add("loader");
+// 2. create a var called 'cardList' that selects the ul with the class of card-list
+let cardList = document.querySelector(".card-list");
+
+function removePlaceholderCards() {
+  // 3. using a for loop loop through each of the loadingCards and remove them from the page.
+  for (let i = 0; i < loadingCards.length; i++) {
+    loadingCards[i].remove();
+  }
+}
+
+function displayLoadedData() {
+  data.forEach((item) => {
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let header = document.createElement("div");
+    header.classList.add("header");
+
+    let img = document.createElement("img");
+    img.setAttribute("src", item.img);
+    header.innerText = item.title;
+    card.appendChild(header);
+    card.appendChild(img);
+    cardList.appendChild(card);
+  });
 }
